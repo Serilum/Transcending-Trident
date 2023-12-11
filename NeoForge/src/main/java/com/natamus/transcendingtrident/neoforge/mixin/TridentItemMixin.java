06966e73e7560a2f874dba456526a7e3e403fed7
Rick South <rick@natamus.com>
2023-12-11 23:54:54 +0100

    Transcending Trident source code for 1.20.2.

---
+++ NeoForge/src/main/java/com/natamus/transcendingtrident/neoforge/mixin/TridentItemMixin.java
@@ -0,0 +1,18 @@
+package com.natamus.transcendingtrident.neoforge.mixin;
+
+import com.natamus.transcendingtrident.config.ConfigHandler;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(value = TridentItem.class, priority = 1001)
+public class TridentItemMixin {
+	@ModifyVariable(method = "releaseUsing(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;I)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/Mth;sqrt(F)F"), ordinal = 1)
+	private int TridentItem_releaseUsing(int k, ItemStack itemStack, Level level, LivingEntity livingEntity, int i) {
+		return Math.round(k*(float)ConfigHandler.tridentUsePowerModifier);
+	}
+}
