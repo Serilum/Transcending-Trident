cfa61054bb01d1c465dead09e6bd8333057ccdc4
Rick South <rick@natamus.com>
2023-12-22 18:10:27 +0100

    (v4.6) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/transcendingtrident/util/Reference.java
+++ Common/src/main/java/com/natamus/transcendingtrident/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.transcendingtrident.util;
 public class Reference {
 	public static final String MOD_ID = "transcendingtrident";
 	public static final String NAME = "Transcending Trident";
-	public static final String VERSION = "4.5";
+	public static final String VERSION = "4.6";
 	public static final String ACCEPTED_VERSIONS = "[1.20.2]";
 }
