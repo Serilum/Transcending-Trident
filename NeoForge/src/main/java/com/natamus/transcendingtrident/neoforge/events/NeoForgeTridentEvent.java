b14ae87a2eb018c1a4ec3e3007ae85b2e45ff826
Rick South <rick@natamus.com>
2025-02-12 23:10:08 +0100

    (v4.9) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/transcendingtrident/neoforge/events/NeoForgeTridentEvent.java
+++ NeoForge/src/main/java/com/natamus/transcendingtrident/neoforge/events/NeoForgeTridentEvent.java
@@ -4,10 +4,8 @@ import com.natamus.transcendingtrident.events.TridentEvent;
 import net.minecraft.world.InteractionResult;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
 
-@EventBusSubscriber
 public class NeoForgeTridentEvent {
 	@SubscribeEvent
 	public static void onItem(PlayerInteractEvent.RightClickItem e) {
