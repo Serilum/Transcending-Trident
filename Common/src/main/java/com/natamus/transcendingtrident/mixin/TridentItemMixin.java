10ee6fd6437a1794e4a6a4015a6e24bc24184308
Rick South <rick@natamus.com>
2025-07-01 09:51:09 +0200

    (v5.0) Fixed an incorrect mixin reference.

--- Common/src/main/java/com/natamus/transcendingtrident/mixin/TridentItemMixin.java
+++ Common/src/main/java/com/natamus/transcendingtrident/mixin/TridentItemMixin.java
@@ -13,7 +13,7 @@ import net.minecraft.world.level.Level;
 
 @Mixin(value = TridentItem.class, priority = 1001)
 public class TridentItemMixin {
-	@ModifyVariable(method = "releaseUsing(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;I)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/Mth;sqrt(F)F"), ordinal = 1)
+	@ModifyVariable(method = "releaseUsing", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/util/Mth;sqrt(F)F"), ordinal = 1)
 	private int TridentItem_releaseUsing(int k, ItemStack itemStack, Level level, LivingEntity livingEntity, int i) {
 		return Math.round(k*(float)ConfigHandler.tridentUsePowerModifier);
 	}
