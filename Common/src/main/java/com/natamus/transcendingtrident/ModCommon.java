e042bdef1c142340ad2bc749351758550c08a218
Rick South <rick@natamus.com>
2026-04-02 15:53:34 +0200

    Transcending Trident source code for 26.1.1.

---
+++ Common/src/main/java/com/natamus/transcendingtrident/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.transcendingtrident;
+
+import com.natamus.transcendingtrident.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
