b92bf5ed11ac70cafbd3eaa7cc9e293584aa7d7e
Rick South <rick@natamus.com>
2026-06-18 21:15:44 +0200

    Updated Transcending Trident to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/transcendingtrident/util/Reference.java
+++ Common/src/main/java/com/natamus/transcendingtrident/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "transcendingtrident";
 	public static final String NAME = "Transcending Trident";
 	public static final String VERSION = "5.0";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
